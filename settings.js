7fa8366f711e7dc4afb6247077b9b9978fe9b057
privacykingimranos-plus <privacykingimranos@hotmail.com>
2026-05-30 16:12:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,15 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Sup',
+  author: '‎Bot',
+  botName: "Sup Bot",
+  botOwner: 'Ahmed Lamtalssi', // Your name
+  ownerNumber: '212607361454', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.7",
+  version: "1.0.0",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
 
